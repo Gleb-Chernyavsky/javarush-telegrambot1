c40ca7b5b12881355c5d3849985ec3fc8196899e
Gleb <g.chernyavskiy@okenit.ru>
2021-07-15 11:23:07 +0300

    Initial commit

---
+++ src/main/java/com/example/javarushtelegrambot1/JavarushTelegrambot1Application.java
@@ -0,0 +1,13 @@
+package com.example.javarushtelegrambot1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavarushTelegrambot1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavarushTelegrambot1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javarushtelegrambot1/JavarushTelegrambot1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javarushtelegrambot1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavarushTelegrambot1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
